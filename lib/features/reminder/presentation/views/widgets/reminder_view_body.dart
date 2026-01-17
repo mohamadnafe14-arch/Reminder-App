a13652017e1f6e1c767ead4d6237c32973f6cb02
mohamed <mnafe44@gmail.com>
2026-01-17 14:58:03 +0200

    finishing reminder view

--- lib/features/reminder/presentation/views/widgets/reminder_view_body.dart
+++ lib/features/reminder/presentation/views/widgets/reminder_view_body.dart
@@ -1,10 +1,12 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:reminder_app/features/reminder/data/models/reminder_model.dart';
 import 'package:reminder_app/features/reminder/presentation/cubit/reminder_cubit/reminder_cubit.dart';
 import 'package:reminder_app/features/reminder/presentation/views/widgets/custom_reminder_list_view.dart';
-import 'package:reminder_app/features/reminder/presentation/views/widgets/reminder_item.dart';
+import 'package:reminder_app/features/reminder/presentation/views/widgets/reminder_view_failure_body.dart';
+import 'package:reminder_app/features/reminder/presentation/views/widgets/reminder_view_initial_body.dart';
+import 'package:reminder_app/features/reminder/presentation/views/widgets/reminder_view_loading_body.dart';
 
 class ReminderViewBody extends StatelessWidget {
   const ReminderViewBody({super.key});
@@ -13,25 +15,15 @@ class ReminderViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ReminderCubit, ReminderState>(
       builder: (BuildContext context, ReminderState state) {
-        return CustomReminderListView(
-          reminders: [
-            ReminderModel(
-              title: "title",
-              description: "description",
-              dateTime: DateTime.now(),
-            ),
-            ReminderModel(
-              title: "title",
-              description: "description",
-              dateTime: DateTime.now(),
-            ),
-            ReminderModel(
-              title: "title",
-              description: "description",
-              dateTime: DateTime.now(),
-            ),
-          ],
-        );
+        if (state is ReminderLoading) {
+          return const ReminderViewLoadingBody();
+        } else if (state is ReminderLoaded) {
+          return CustomReminderListView(reminders: state.reminders);
+        } else if (state is ReminderError) {
+          return ReminderViewFailureBody(errorMessage: state.message);
+        } else {
+          return const ReminderViewInitialBody();
+        }
       },
     );
   }
